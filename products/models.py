82c2fe033d3ad84a1760c002e7b887b4133ab0e4
zsrichock <saza_thi@hotmail.com>
2016-04-26 16:57:45 +0700

    begin project

---
+++ products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):  ##def __str__(self):
+        return self.title
+        # slug
+        # inventory?
+
+
+
+# Product Images
+
+
+# Product Category
---
+++ products/views.py
@@ -0,0 +1,34 @@
+from django.views.generic.detail import DetailView
+from django.shortcuts import render, get_object_or_404
+from django.http import  Http404
+
+# Create your views here.
+
+
+from .models import  Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    # template_name  = "<appname>/modelname_detail.html"
+
+
+def product_detail_view_func(request, id):
+
+    product_instance = Product.objects.get(id=id)
+    product_instance = get_object_or_404(Product,id=id)
+    try:
+        product_instance = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404
+    except:
+        raise Http404
+
+
+
+    template = "products/product_detail.html"
+    context = {
+        "object": product_instance
+    }
+
+    return render(request, template, context)
