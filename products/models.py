10a49dba641c6fdbc1e305d79afb600706bd5471
zsrichock <saza_thi@hotmail.com>
2016-05-10 23:15:50 +0700

    begin project

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -40,7 +41,6 @@ class Product(models.Model):
 
         # Product Category
 
-
 class Variation(models.Model):
     product = models.ForeignKey(Product)
     title = models.CharField(max_length=120)
@@ -76,7 +76,10 @@ def product_saved_receiver(sender, instance, created, *args, **kwargs):
     #variations = product.objects.filter(product=product)
     #print created
 
-post_save.connect(product_saved_receiver,sender=Product)
+
+post_save.connect(product_saved_receiver, sender=Product)
+
+
 #
 # class Test(models.Model):
 #     aname = models.CharField(max_length=120)
@@ -85,10 +88,18 @@ post_save.connect(product_saved_receiver,sender=Product)
 #     def __unicode__(self):
 #         return self.aname
 
+def image_upload_to(instance, filename):
+    title = instance.product.title
+    slug = slugify(title)
+    basename, file_extension = filename.split(".")
+    new_filename = "%s-%s.%s"  %(slug, instance.id, file_extension)
+    return "products/%s/%s" %(slug, new_filename)
+
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product)
-    image = models.ImageField(upload_to='products/')
+    #image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to=image_upload_to)
 
     def __unicode__(self):
         return self.product.title
